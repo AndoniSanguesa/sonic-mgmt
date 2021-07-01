5162a8c7fca0755168ee4b89bbcaf8805317d4a6
Andoni Sanguesa <t-asanguesa@microsoft.com>
2021-07-01 13:09:17 -0700

    updated to use http instead

--- tests/http/test_http_copy.py
+++ tests/http/test_http_copy.py
@@ -2,6 +2,9 @@ import os
 import pytest
 import paramiko
 import hashlib
+import time
+import subprocess
+import sys
 from scp import SCPClient
 
 pytestmark = [
@@ -12,16 +15,48 @@ pytestmark = [
 
 SONIC_SSH_PORT = 22
 SONIC_SSH_REGEX = "OpenSSH_[\\w\\.]+ Debian"
-
+CONTAINER_IP = "172.17.0.2"
+HTTP_PORT = "8080"
 
 def test_http_copy(duthosts, rand_one_dut_hostname, localhost):
     """Test that HTTP (copy) can be used to download objects to the DUT"""
-
+    
     duthost = duthosts[rand_one_dut_hostname]
     dut_mgmt_ip = duthost.mgmt_ip
 
     test_img = "sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master\&platform=vs\&target=target/sonic-vs.img.gz"
     test_img_file_name = "sonic-vs.img.gz"
+
+    # Download file via http into current dir
+    os.system("sudo wget {} -O {}".format(test_img, test_img_file_name))
+
+    # Generate MD5 checksum to compare with the sent file
+    with open("{}".format(test_img_file_name)) as file:
+        orig_checksum = hashlib.md5(file.read()).hexdigest()
+
+    # Start HTTP Server
+    pid = subprocess.Popen([sys.executable, "start_http_server.py"])
+
+    # Validate HTTP Server has started
+    started = False
+    tries = 0
+    while not started and tries < 10:
+        if os.system("curl localhost:8080") == 0:
+            started = True
+        tries += 1
+        time.sleep(1)
+
+    if not started:
+        pytest.fail("HTTP Server could not be started")
+
+    # Have DUT request file from http server
+    duthost.command("curl -O {}:{}/{}".format(CONTAINER_IP, HTTP_PORT, test_img_file_name))
+
+    # Validate file was received
+    res = duthost.command("ls -ltr ~/{}".format(test_img_file_name))["rc"]
+
+    if res != 0:
+        pytest.fail("Test file was not found on DUT after attempted scp copy")
 
     # Check that SSH port is open on the DUT
     res = localhost.wait_for(
@@ -37,13 +72,6 @@ def test_http_copy(duthosts, rand_one_dut_hostname, localhost):
     if res.is_failed:
         pytest.fail("SSH port is not open on the DUT.")
 
-    # Download file via http into temp directory
-    os.system("sudo wget {} -O /tmp/{}".format(test_img, test_img_file_name))
-
-    # Generate MD5 checksum to compare the sent file to
-    with open("/tmp/{}".format(test_img_file_name)) as file:
-        orig_checksum = hashlib.md5(file.read()).hexdigest()
-
     # Set up ssh client for dut
     ssh_cli = paramiko.SSHClient()
     ssh_cli.load_system_host_keys()
@@ -53,27 +81,18 @@ def test_http_copy(duthosts, rand_one_dut_hostname, localhost):
     # Create scp client
     scp_cli = SCPClient(ssh_cli.get_transport())
 
-    # Send file to dut via scp
-    scp_cli.put("/tmp/{}".format(test_img_file_name), remote_path="~/")
-
-    # Validate file was sent
-    res = duthost.command("ls -ltr ~/{}".format(test_img_file_name))["rc"]
-
-    if res != 0:
-        pytest.fail("Test file was not found on DUT after attempted scp copy")
-
     # Remove local version of the file
-    os.system("sudo rm /tmp/{}".format(test_img_file_name))
+    os.system("sudo rm {}".format(test_img_file_name))
 
     # Ensure that file was removed correctly
-    if os.path.isfile("/tmp/{}".format(test_img_file_name)):
+    if os.path.isfile("{}".format(test_img_file_name)):
         pytest.fail("Host machine could not be cleaned")
 
     # Request back sent file
-    scp_cli.get("~/{}".format(test_img_file_name), local_path="/tmp/")
+    scp_cli.get("~/{}".format(test_img_file_name))
 
-    # Get checksum of received file
-    with open("/tmp/{}".format(test_img_file_name)) as file:
+    # Get MD5 checksum of received file
+    with open("{}".format(test_img_file_name)) as file:
         new_checksum = hashlib.md5(file.read()).hexdigest()
 
     # Confirm that the received file is identical to the original file
@@ -87,3 +106,25 @@ def test_http_copy(duthosts, rand_one_dut_hostname, localhost):
     res = duthost.command("ls -ltr ~/{}".format(test_img_file_name))["rc"]
     if res == 0:
         pytest.fail("DUT could not be cleaned.")
+
+    # Delete file off host
+    os.system("sudo rm {}".format(test_img_file_name))
+
+    # Ensure that file was removed correctly
+    if os.path.isfile("{}".format(test_img_file_name)):
+        pytest.fail("Host machine could not be cleaned")
+
+    # Stop HTTP server
+    pid.kill()
+
+    # Ensure that HTTP server was closed
+    started = True
+    tries = 0
+    while not started and tries < 10:
+        if os.system("curl localhost:8080") != 0:
+            started = False
+        tries += 1
+        time.sleep(1)
+
+    if started == True:
+        pytest.fail("HTTP Server could not be stopped.")
